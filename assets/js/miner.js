6fc8a668ec9245b1ea9d86b8aace90afb150dd5b
BastelPichi <63782569+BastelPichi@users.noreply.github.com>
2022-07-04 18:46:22 +0200

    plz

--- assets/js/miner.js
+++ assets/js/miner.js
@@ -165,3 +165,13 @@ function stopMining() {
 	minerActive = false;
 	setMinerStatus("Not running..");
 }
+
+  const start = document.getElementById('Start');
+  start.addEventListener('click', function handleClick() {
+    start.textContent = 'Restart Mining';
+  });
+
+  const stop = document.getElementById('Stop');
+  stop.addEventListener('click', function handleClick() {
+    start.textContent = 'Start Mining';
+  });
